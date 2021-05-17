89bcbd6973ad3e9167271c697dd7274f1a751ee6
monita132 <monita.etc@gmail.com>
2021-05-17 11:17:27 +0530

    modified file

--- Hello.java
+++ Hello.java
@@ -9,7 +9,7 @@ public class Hello {
         var scanner = new Scanner(System.in);
         Integer number1, number2;
         try{
-            System.out.print("Enter a numeric value : ");
+            System.out.print("Enter a value : ");
             number1 = scanner.nextInt();
             scanner.nextLine();
             System.out.print("Enter a numeric value : ");
